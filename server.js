ef64de3d05a246ba81dd3fccb5a4da527f889fe9
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2023-10-29 15:03:48 -0700

    initial

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models"); // takes ./apps/models/index.js like __init__.py for a folder
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "JWT authentication here!" });
+});
+
+// routes
+require("./app/routes/auth")(app);
+require("./app/routes/user")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+}); 
+
+function initial() {
+  
+  // initiates roles
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({ 
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
